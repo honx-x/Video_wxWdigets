f2a4f3cf84fb1bb0ec6cc83b16d971d35540bc78
Hi_HONG <Hi_HONG@DESKTOP-60RNGHR>
2020-09-16 18:13:45 +0900

    2020-09-16

--- Video_wxWdigets/cMain.cpp
+++ Video_wxWdigets/cMain.cpp
@@ -1,4 +1,5 @@
 #include "cMain.h"
+#include <wx/listctrl.h>
 #include <chrono>
 using namespace std;
 
@@ -9,22 +10,15 @@ EVT_BUTTON(10002, Btn_resetClicked)
 EVT_BUTTON(10003, Btn_saveClicked)
 EVT_BUTTON(10004, Btn_deleteClicked)
 
+
 wxEND_EVENT_TABLE();
 
-//wxUSE_LISTCTRL(cMain, wxListbox)
-
-
-
+//wxUSE_LISTCTRL(cMain, wxListCtrl)
 
 
 cMain::cMain() : wxFrame(nullptr, wxID_ANY, "HiMES - 2020", wxPoint(300, 300), wxSize(1024, 768))
 {
 
-
-	time_t timer = time(NULL);
-	tm* local_time = localtime(&timer);
-	std::string nowtime = std::to_string(local_time->tm_hour) + " 시 " + std::to_string(local_time->tm_min) + " 분 " + std::to_string(local_time->tm_sec) + " 초";
-	wxString st1(nowtime);
 
 	m_st1 = new wxStaticText(this, wxID_ANY, "라벨번호 : ", wxPoint(30, 35));
 	m_st2 = new wxStaticText(this, wxID_ANY, "원자제명 : ", wxPoint(30, 75));
@@ -38,9 +32,10 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "HiMES - 2020", wxPoint(300, 300), w
 	m_tc4 = new wxTextCtrl(this, 20004, "", wxPoint(100, 150), wxSize(300, 30));
 	m_tc5 = new wxTextCtrl(this, 20005, "", wxPoint(100, 190), wxSize(300, 30));
 
-	m_list1 = new wxListBox(this, wxID_ANY, wxPoint(450, 30), wxSize(500, 600));
+	//m_list1 = new wxListBox(this, wxID_ANY, wxPoint(450, 30), wxSize(500, 600));
 	m_list2 = new wxListBox(this, wxID_ANY, wxPoint(30, 241), wxSize(370, 390));
-
+	//m_lc1 = new wxListCtrl(this, wxID_ANY, wxPoint(450, 30), wxSize(500, 600));
+	m_lc1 = new wxListCtrl(this, wxID_ANY, wxPoint(450, 30), wxSize(500, 600),wxLC_REPORT);
 
 	m_btn1 = new wxButton(this, 10001, "추가", wxPoint(35, 650), wxSize(170, 40));
 	m_btn2 = new wxButton(this, 10002, "리셋", wxPoint(220, 650), wxSize(170, 40));
@@ -49,64 +44,60 @@ cMain::cMain() : wxFrame(nullptr, wxID_ANY, "HiMES - 2020", wxPoint(300, 300), w
 	m_btn4 = new wxButton(this, 10004, "삭제", wxPoint(720, 650), wxSize(200, 40));
 
 
+
+
 	m_tc1->SetHint(" ex) 111544722");
 	m_tc2->SetHint(" ex) 제품명");
 	m_tc3->SetHint(" ex) LCD1101");
 	m_tc4->SetHint(" ex) 100");
 	m_tc5->SetHint(" ex) 1");
 
+	
+	m_lc1->InsertColumn(0, "LabelNo",wxLIST_AUTOSIZE);
+	m_lc1->InsertColumn(1, "원자제명",wxLIST_AUTOSIZE);
+	m_lc1->InsertColumn(2, "제품코드",wxLIST_AUTOSIZE);
+	m_lc1->InsertColumn(3, "중량(kg)",wxLIST_AUTOSIZE);
+	m_lc1->InsertColumn(4, "수량(ea)",wxLIST_AUTOSIZE);
+
+
 }
 
 cMain::~cMain()
 {
 
+}
 
+
+void cMain::Now_time() // 현재시간 출력
+{
+
+	time_t timer = time(NULL);
+	tm* local_time = localtime(&timer);
+	std::string nowtime(std::to_string(local_time->tm_hour) + " 시 " + std::to_string(local_time->tm_min) + " 분 " + std::to_string(local_time->tm_sec) + " 초");
+
+	//st1(nowtime.c_str(), wxConvUTF8);
+	st1 = nowtime;
+
+	return;
 
 }
 
 void cMain::Btn_addClicked(wxCommandEvent& evt1) // 추가버튼 클릭시 이벤트
-
-
-
 {
-
-	//
-	/*
-	if (m_tc1->IsEmpty() && m_tc2->IsEmpty() && m_tc3->IsEmpty() && m_tc4->IsEmpty()) {
-
-
-		m_list2->AppendString(m_tc1->GetValue());
-		evt1.Skip();
-	}
-	else
-	{
-		evt1.Skip();
-	}
-	*/
-
-
-	/*
-	printf("유닉스 타임 (Unix Time): %d 초\n\n", timer); // 1970년 1월 1일 0시 0분 0초부터 시작하여 현재까지의 초
-
-	printf("현재 년: %d\n", t->tm_year + 1900);
-	printf("현재 월: %d\n", t->tm_mon + 1);
-	printf("현재 일: %d\n\n", t->tm_mday);
-
-	printf("현재 시: %d\n", t->tm_hour);
-	printf("현재 분: %d\n", t->tm_min);
-	printf("현재 초: %d\n\n", t->tm_sec);
-
-	printf("현재 요일: %d\n", t->tm_wday); // 일요일=0, 월요일=1, 화요일=2, 수요일=3, 목요일=4, 금요일=5,토요일 = 6
-	printf("올해 몇 번째 날: %d\n", t->tm_yday); // 1월 1일은 0, 1월 2일은 1
-	printf("서머타임 적용 여부: %d\n", t->tm_isdst); // 0 이면 서머타임 없음
-	*/
-
-	//CString sTmp;
-	//sTmp.Format("[%s]  라벨번호 :", st1, m_tc1->GetValue());
-	//m_list2->AppendString("["+st1+"]  라벨번호 : " + m_tc1->GetValue() + "가 추가되었습니다.");
+	Now_time();
 	//m_list2->AppendString(st1);
 
+
+	int idx = 0;
+	m_lc1->InsertItem(idx, m_tc1->GetValue(),0);
+	idx++;
+	
+
+
+	m_list2->AppendString("[" + st1 + "]  라벨번호 : " + m_tc1->GetValue() + "가 추가되었습니다.");
+	m_tc1->SetFocus();
 	evt1.Skip();
+
 
 }
 
@@ -118,6 +109,9 @@ void cMain::Btn_resetClicked(wxCommandEvent& evt2) // 리셋버튼 클릭시 이벤트
 	m_tc4->SetValue("");
 	m_tc5->SetValue("");
 
+
+	Now_time();
+	m_list2->AppendString("[" + st1 + "]  초기화 되었습니다.");
 	wxMessageBox("초기화 되었습니다.");
 	m_tc1->SetFocus();
 
--- Video_wxWdigets/cMain.h
+++ Video_wxWdigets/cMain.h
@@ -1,20 +1,19 @@
 #pragma once
 #include "wx/wx.h"
+#include "wx/listctrl.h"
+#include "wx/listbase.h"
 
 
 class cMain : public wxFrame
-
-
 {
-
 public:
+
 	cMain();
 	~cMain();
 
-public:
 
-	wxString* st1 = nullptr;
-	//string* nowtime = nullptr;
+	wxString st1;
+	wxString nowtime;
 
 	wxStaticText* m_st1 = nullptr;
 	wxStaticText* m_st2 = nullptr;
@@ -22,10 +21,10 @@ public:
 	wxStaticText* m_st4 = nullptr;
 	wxStaticText* m_st5 = nullptr;
 
-	wxButton * m_btn1 = nullptr;
-	wxButton * m_btn2 = nullptr;
-	wxButton * m_btn3 = nullptr;
-	wxButton * m_btn4 = nullptr;
+	wxButton* m_btn1 = nullptr;
+	wxButton* m_btn2 = nullptr;
+	wxButton* m_btn3 = nullptr;
+	wxButton* m_btn4 = nullptr;
 
 	wxTextCtrl* m_tc1 = nullptr;
 	wxTextCtrl* m_tc2 = nullptr;
@@ -35,15 +34,19 @@ public:
 
 	wxListBox* m_list1 = nullptr;
 	wxListBox* m_list2 = nullptr;
-	
-	
 
+	wxListCtrl* m_lc1 = nullptr;
+
+
+
+	void Now_time();
 	void Btn_addClicked(wxCommandEvent& evt1);
 	void Btn_resetClicked(wxCommandEvent& evt2);
 	void Btn_saveClicked(wxCommandEvent& evt3);
 	void Btn_deleteClicked(wxCommandEvent& evt4);
 
 	wxDECLARE_EVENT_TABLE();
+
 };
 
 
